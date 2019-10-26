d41cad283c36e336a092471a022bd2327a53ac08
scarferok <scarferok@gmail.com>
2019-10-26 18:36:32 +0300

    react min conf

---
+++ react-1/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    devServer:{
+        contentBase: path.join(__dirname,'/dist'),
+        compress: true,
+        historyApiFallback: true,
+        host: '127.0.0.1',
+        port: 8080,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+               test: /\.(png|svg|jpg|gif)$/,
+               use: [
+                   'file-loader'
+               ],
+            },
+            {
+                test: /\.(js|jsx|ts)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+        ],
+    },
+};
