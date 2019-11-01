65f6b5dbb3d3195f8cd2ed45567039442c663fef
scarferok <scarferok@gmail.com>
2019-11-01 16:18:40 +0300

    Weather app

--- react-minimal-project/src/App.tsx
+++ react-minimal-project/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {Weather} from './Weather'
 
 export function App() {
     return(
         <div>
             <h1>Weather app</h1>
+            <Weather/>
         </div>
     )
 }
